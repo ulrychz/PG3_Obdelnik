652a1c9d5b5fefaf4982fabf46c5213c1c703b91
ulrych <ulrych@hotmail.cz>
2024-04-04 19:55:02 +0200

    Přidejte soubory projektu.

---
+++ PG3_Obdelnik/Models/Obdelnik.cs
@@ -0,0 +1,86 @@
+﻿namespace PG3_Obdelnik.Models
+{
+	public class Obdelnik
+	{
+		#region Vlastnosti
+		private int stranaA = 10;
+		private int stranaB = 10;
+		private int maxRozmer = 100;
+
+		public int MaxRozmer
+		{
+			get => maxRozmer; set
+			{
+				//prevedeno na kladne cislo
+				maxRozmer = Math.Abs(value);
+				if (maxRozmer < 10) { maxRozmer = 10; }
+
+				//kontrola na max rozmer pro strany a, b
+				if (StranaA > maxRozmer)
+				{
+					stranaA = maxRozmer;
+				}
+				if (StranaB > maxRozmer)
+				{
+					stranaB = maxRozmer;
+				}
+
+			}
+		}
+		public int StranaA
+		{
+			get => stranaA;
+			set
+			{
+				if (stranaA != value)
+				{
+					if (value > MaxRozmer)
+					{
+						stranaA = MaxRozmer;
+					}
+					else if (value < 1)
+					{
+						stranaA = 1;
+					}
+					else
+					{
+						stranaA = value;
+					}
+				}
+			}
+		}
+		public int StranaB
+		{
+			get => stranaB; set
+			{
+				if (stranaB != value)
+				{
+					if (value > MaxRozmer)
+					{
+						stranaB = MaxRozmer;
+					}
+					else if (value < 1)
+					{
+						stranaB = 1;
+					}
+					else
+					{
+						stranaB = value;
+					}
+				}
+			}
+		}
+
+		public int Obsah => StranaA * StranaB;
+		public int Obvod => 2 * (StranaA + StranaB);
+		public double Uhlopricka => Math.Sqrt(StranaA * StranaA + Math.Pow(StranaB, 2));
+		#endregion
+
+		#region Metody
+		private void Pokus()
+		{
+
+		}
+		#endregion
+	}
+}
